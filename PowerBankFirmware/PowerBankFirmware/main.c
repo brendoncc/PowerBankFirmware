382fea178837a0aad215c77999d1312f6225e40f
brendoncc <45137600+brendoncc@users.noreply.github.com>
2020-08-14 13:44:39 +1200

    Created Atmel Studio Project
    Uses ATTiny 2313A (current on hand uC, change to ATTiny44a when PCB is built).
    
    Using to test commits and branches, will be starting point for firmware

---
+++ PowerBankFirmware/PowerBankFirmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * PowerBankFirmware.c
+ *
+ * Created: 14/08/2020 1:42:05 PM
+ * Author : Brendon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
