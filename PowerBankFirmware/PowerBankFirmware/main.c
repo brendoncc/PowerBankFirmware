0f6fd2d9bb17e53b50b5e850cae033f1dc3b22e9
brendoncc <45137600+brendoncc@users.noreply.github.com>
2020-08-15 00:15:12 +1200

    Added LED toggle and testing downloading firmware to test uC

--- PowerBankFirmware/PowerBankFirmware/main.c
+++ PowerBankFirmware/PowerBankFirmware/main.c
@@ -13,6 +13,9 @@ void setup(void);
 ISR (TIMER1_COMPA_vect);
 ISR (TIMER0_COMPA_vect);
 
+//bit masking of IO
+#define LED_TOGGLE (PORTB ^= (1<<0))
+
 int main(void)
 {
 	setup();
@@ -23,20 +26,30 @@ int main(void)
     }
 }
 
+ISR (TIMER0_COMPA_vect)	//timer based interrupt that triggers on compare match of OCR0A and TCNT0
+{
+
+}
+
+ISR (TIMER1_COMPA_vect) //timer based interrupt that triggers on compare match of OCR1A and TCNT1
+{
+	LED_TOGGLE;
+}
+
 void setup(void)
 {
-	DDRA = 0x00;		//inputs
-	PORTA = 0x07;		//enable pullup resistors on bit 0 & 1 & 3
-	DDRB = 0xFF;		//set portb to output
-	PORTB = 0xFF;		//outputs initially high.
-	DDRD = 0x60;		//bits 5 & 6 set as output.
-	PORTD = 0xFF;		//bits 5 & 6 initially high, leds off. inputs pulled up
-	TCCR0A = 0x02;		//8 bit timer in CTC mode
-	TCCR0B = 0x04;		//prescaler 256, 256us
-	OCR0A = 156.24;		//interrupt every 5ms (125us * 156.24 = 5ms)
-	TCCR1A = 0x00;		//16 bit timer in CTC mode
-	TCCR1B = 0x0C;		//prescaler 256, 256us.
-	OCR1A = 31250;		//interrupt every 1s (125us * 31250 = 1s)
-	TIMSK = 0x41;		//Compare match on OCR1A(TCNT1) and OCR0A(TCNT0)
-	sei();				//global interrupts enabled
+	DDRA = 0b00000000;		//Set register A to all inputs (0)
+	PORTA = 0b11111111;		//enable all PORTA pull up resistors (1 enable , 0 disable)
+	DDRB = 0b11111111;		//Set register B to all outputs (1)
+	PORTB = 0b00000000;		//all PORTB outputs initially low (1 high, 0 low)
+	DDRD = 0b00000000;		//Set register D to all inputs (0)
+	PORTD = 0b11111111;		//enable all PORTD pull up resistors (1 enable, 0 disable)
+	TCCR0A = 0x02;			//8 bit timer in CTC mode
+	TCCR0B = 0x04;			//prescaler 256, 256us
+	OCR0A = (31250 / 1000);			//interrupt every 1ms
+	TCCR1A = 0x00;			//16 bit timer in CTC mode
+	TCCR1B = 0x0C;			//prescaler 256, 256us.
+	OCR1A = (31250);			//interrupt every 1s (8000000/256/31250 = 1)
+	TIMSK = 0x41;			//Compare match on OCR1A(TCNT1) and OCR0A(TCNT0)
+	sei();					//global interrupts enabled
 }
