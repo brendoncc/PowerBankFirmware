518286dff09e6435aee357550dbaff97492b81e2
brendoncc <45137600+brendoncc@users.noreply.github.com>
2026-04-18 18:04:29 +0100

    Updated .gitignore to clean repo

--- PowerBankFirmware/PowerBankFirmware/Debug/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-PowerBankFirmware.c
-
-USI_TWI_Master.c
-
